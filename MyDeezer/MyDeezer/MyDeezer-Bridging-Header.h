75b3b015a66221c017d057ee59cf0531b1f1bc60
Reda Mimouni <Reda Mimouni>
2016-10-01 15:20:47 +0200

    Adding Bridge file to access Objective C SDK

---
+++ MyDeezer/MyDeezer/MyDeezer-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  MyDeezer-Bridging-Header.h
+//  MyDeezer
+//
+//  Created by Dev on 01/10/2016.
+//  Copyright © 2016 Dev. All rights reserved.
+//
+
+#ifndef MyDeezer_Bridging_Header_h
+#define MyDeezer_Bridging_Header_h
+
+#import "DezTest.h"
+#import "DeezerConnect.h"
+
+#endif /* MyDeezer_Bridging_Header_h */
